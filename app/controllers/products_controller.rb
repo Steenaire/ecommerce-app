b8e2f0e0df3c3796ee7108ffd01e3ce73fea5d1c
Steen Clarke <cmclarkey@gmail.com>
2016-07-24 16:29:50 -0700

    Add 'new' page for creating new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,21 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @name = params[:name]
+    @price = params[:price]
+    @description = params[:description]
+    @image = params[:image]
+    @quantity = params[:quantity]
+    @category = params[:category]
+
+    product = Product.new({
+      name: @name, price: @price, description: @description, image: @image, quantity: @quantity, category: @category
+      })
+    product.save
+  end
+
 end
