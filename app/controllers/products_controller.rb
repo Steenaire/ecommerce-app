754110aba6282856b978d1e7f9ff2151f794f8aa
Steen Clarke <cmclarkey@gmail.com>
2016-07-24 17:02:56 -0700

    Add edit products functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,4 +25,28 @@ class ProductsController < ApplicationController
     product.save
   end
 
+  def update
+    @name = params[:name]
+    @price = params[:price]
+    @description = params[:description]
+    @image = params[:image]
+    @quantity = params[:quantity]
+    @category = params[:category]
+
+    product = Product.find(params[:id])
+
+    product.name = @name
+    product.price = @price
+    product.description = @description
+    product.image = @image
+    product.quantity = @quantity
+    product.category = @category
+    product.save
+  end
+
+  def edit
+    @id = params[:id]
+    @product = Product.find(@id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new'
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show'
+  get '/products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
 end
