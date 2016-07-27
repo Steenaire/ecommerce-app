19894d45c8881e46539d972eef91c315465eccd3
Steen Clarke <cmclarkey@gmail.com>
2016-07-26 21:18:42 -0700

    Add discount method, as well as styling for discount items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    if price < 2
+      return "Discount item!"
+    else
+      return "On Sale!"
+    end
+  end
+
+  def tax
+    price.to_f*0.09
+  end
+
+  def total
+    price + self.tax
+  end
+
 end
