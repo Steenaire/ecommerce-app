080d0ebe6698323a3465f93e6b7be5ee37e3fe7c
Steen Clarke <cmclarkey@gmail.com>
2016-07-27 13:55:52 -0700

    Add instance methods for product, sale_message, tax calculation, and total price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    if price < 2
+      return "Discount item!"
+    else
+      return "On Sale!"
+    end
+  end
+
+  def tax
+    (price.to_f*0.09).round(2)
+  end
+
+  def total
+    (price + self.tax).round(2)
+  end
+
 end
