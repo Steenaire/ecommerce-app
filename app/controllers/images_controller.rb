1548a2b5d9899b3613a10add97ddcf3f8a704e8e
Steen Clarke <cmclarkey@gmail.com>
2016-07-31 15:31:20 -0700

    Edit images controller for adding images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,15 +4,19 @@ class ImagesController < ApplicationController
   end
 
   def create
-    image = Image.new({
-      url: params[:url]
-      product_id: product.id
-      })
+    image = Image.new({product_params})
+    image.product_id = product.id
     product.save
 
     flash[:success] = "Image Added!"
 
     redirect_to "/products/#{product.id}"
+  end
+
+  private
+
+  def product_params
+    params.require(:image).permit(:url, :product_id)
   end
 
 end
